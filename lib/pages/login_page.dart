025b20a0501da83d79c43a3149c9cf74453dd208
marlonmnz <marlon_menezes@outlook.com>
2023-09-08 00:40:50 -0300

    Feat: Relayout da tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,11 +8,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _passwordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 208, 221, 238),
+        backgroundColor: const Color.fromARGB(255, 13, 5, 20),
         body: SizedBox(
           width: double.infinity,
           child: Column(
@@ -21,13 +23,38 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(
                 height: 70,
               ),
-              Icon(
-                Icons.person,
-                size: 150,
-                color: Colors.blue.shade500,
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Image.network(
+                      'https://hermes.digitalinnovation.one/assets/diome/logo.png',
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
               ),
               const SizedBox(
-                height: 50,
+                height: 40,
+              ),
+              const Text(
+                'Já tem cadastro?',
+                style: TextStyle(fontSize: 38, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'Faça seu login e make the change._',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 80,
               ),
               Container(
                 alignment: Alignment.center,
@@ -40,14 +67,27 @@ class _LoginPageState extends State<LoginPage> {
                   children: [
                     Expanded(
                       flex: 2,
-                      child: Text('Informe seu e-mail:'),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Text('E-mail'),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email_outlined,
+                            color: Colors.purple,
+                          ),
+                          hintText: 'E-mail',
+                          hintStyle: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
                     ),
                   ],
                 ),
+              ),
+              const SizedBox(
+                height: 30,
               ),
               Container(
                 alignment: Alignment.center,
@@ -56,31 +96,92 @@ class _LoginPageState extends State<LoginPage> {
                   horizontal: 30,
                 ),
                 height: 30,
-                child: const Row(
+                child: Row(
                   children: [
                     Expanded(
                       flex: 2,
-                      child: Text('Informe a senha'),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Text('Senha'),
+                      child: TextField(
+                        style: const TextStyle(color: Colors.white),
+                        keyboardType: _passwordVisible
+                            ? TextInputType.visiblePassword
+                            : TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          icon: const Icon(
+                            Icons.lock_outline,
+                            color: Colors.purple,
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                            child: Icon(
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Senha',
+                          hintStyle: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
+              const SizedBox(
+                height: 60,
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Expanded(
+                        flex: 15,
+                        child: TextButton(
+                          onPressed: () {
+                            // Navigator.pushNamed(context, '/home');
+                          },
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            backgroundColor: Colors.purple,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text(
+                            'ENTRAR',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                    ],
+                  )),
               Expanded(
                 child: Container(),
               ),
               Container(
                 alignment: Alignment.center,
-                width: double.infinity,
                 margin: const EdgeInsets.symmetric(
                   horizontal: 30,
                 ),
                 height: 30,
-                color: Colors.green,
-                child: const Text('Login'),
+                child: Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(fontSize: 20, color: Colors.yellow.shade700),
+                ),
               ),
               const SizedBox(
                 height: 10,
@@ -91,10 +192,13 @@ class _LoginPageState extends State<LoginPage> {
                   horizontal: 30,
                 ),
                 height: 30,
-                child: const Text('Cadastre-se'),
+                child: Text(
+                  'Criar conta',
+                  style: TextStyle(fontSize: 20, color: Colors.green.shade500),
+                ),
               ),
               const SizedBox(
-                height: 20,
+                height: 50,
               ),
             ],
           ),
